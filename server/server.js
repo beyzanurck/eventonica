001ba6e497fde8d9346dc67a910d14a5e613f343
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-09-13 07:19:34 -0500

    creates a Post request in the server.js

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const PORT = 8080;
 
 // Configuring cors middleware
 app.use(cors());
+app.use(express.json()); //req.body
 
 // Configuring body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,32 +28,33 @@ app.get('/api/events', async (req, res) =>{
     //real connection with the DB eventonica
     try{
         const { rows: events } = await db.query('SELECT * FROM events');
-        console.log("in the sercer",events)
+        //console.log("in the sercer",events)
         res.send(events);
 
     } catch(error){
         console.log(error);
         return res.status(400).json({error});
 
+    }    
+})
+
+//CREATE AN EVENT
+//Now you can add a new event through Postman. You don't need a UI
+app.post("/api/events", async (req, res) => {
+    try {
+        const {title, location, eventtime} = req.body; 
+
+        const newEvent = await db.query (
+            "INSERT INTO events (title, location, eventtime) VALUES ($1, $2, $3) RETURNING *", //RETURNING:  RETURNS ALL DATA
+                [title, location, eventtime]
+        );
+
+        res.json(newEvent.rows[0]);
     }
-
-    //hardcode the events response for testing reasons. This call has one more event that the real DB 
-    // try{
-    //     const events = [
-
-    //         {id: 1, title: 'Women in Tech Techtonica Panel', location: 'Overland Park Convention Center'},
-    //         {id: 2, title: 'Japanese Cultural Education', location: 'Seattle Convention Center'},
-    //         {id: 3, title: "Haven 90's Party Night Club", location: 'Hilton Hotel Kansas City'},
-    //         {id: 4, title: 'Comedy Night at the Station', location: 'SF Hilton Hotel'},
-    //         {id: 5, title: 'A Decadent Arts Experience', location: 'West Ridge Mall'},
-    //         {id: 6, title: 'Techtonica Classroom Course', location: 'Techtonica HQ'}
-    //       ];
-    //     res.json(events);
-
-    // } catch(error){
-    //     console.log(error);
-    // }   
-    
+    catch (err) {
+        console.error(err.message)
+        res.status(400).json({err});
+    }
 })
 
 
