b394e2850fee0be5e6b50af11b947825a03ac3c8
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-09-14 14:59:06 -0500

    implements input area to add a new event

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,17 @@ import './App.css';
 import Event from './components/event';
 import React, {useState, useEffect} from 'react';
 
+
+
 function App() {
 
   const [events, setEvents] = useState([]);
+
+  const [newEvent, setNewEvent] = useState({
+    title: "",
+    location: "",
+    eventtime: ""
+  });
 
   const getAllEvents = () => {
     fetch("http://localhost:8080/api/events")
@@ -31,6 +39,29 @@ function App() {
 
   useEffect(() => {getAllEvents()}, []);
 
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    try {
+      const { title, location, eventtime } = newEvent;
+      const body = { title, location, eventtime };
+      const response = await fetch("http://localhost:8080/api/events", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body)
+      });
+
+      window.location = "/";
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  function handleChange(event){
+    const { value, name } = event.target;
+    setNewEvent((preValue) => ({ ...preValue, [name]: value }));
+  }
+
   return (
     <div className="App">
     <h1>Techtonica 2023 H2 events</h1>
@@ -43,11 +74,18 @@ function App() {
        : 
       ` `
     }
+
+    <form onSubmit={handleSubmit}>
+        <input name="title" onChange={handleChange} placeholder="Title" />
+        <input name="location" onChange={handleChange} placeholder="Location" />
+        <input name="eventtime" onChange={handleChange} placeholder="MM/DD/YY" />
+
+        <button type="submit">Add Event</button>
+    </form>
     
   </div>
 
   )
-  
 }
 
 export default App
